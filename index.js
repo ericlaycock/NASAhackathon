e26e6da1fe52708c0f30c2ebfb070eb89bf77a04
ericlaycock <ericlaycock44@gmail.com>
2022-10-01 14:30:00 -0400

    Hopefully this doesn't break anything

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import {updateIrradiance} from './updateIrradiance';
 
 var config = {
     type: Phaser.AUTO,
@@ -31,12 +32,7 @@ var max = 100
 var min = 0
 
 var game = new Phaser.Game(config);
-
-function updateIrradiance() {
-
-    fetch('irradiance.csv')
-        .then(response => console.log(response));
-}
+updateIrradiance();
 
 function preload() {
     this.load.image('sky', 'assets/sky.png');
